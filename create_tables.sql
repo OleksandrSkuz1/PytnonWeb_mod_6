66cd17d4f1dcd24f4e984ca6e66e78a5270cd529
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-04-05 18:40:44 +0300

    add

--- create_tables.sql
+++ create_tables.sql
@@ -10,18 +10,19 @@ CREATE TABLE Groups (
 DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
     id SERIAL PRIMARY KEY,
-    fuul_name VARCHAR(150) NOT NULL,
+    full_name VARCHAR(150) NOT NULL,
     group_id INTEGER REFERENCES Groups(id)
-    
+    on delete cascade
+
 );
 
 
 -- Створення таблиці викладачів
-DROP TABLE IF EXISTS Teachers;  
+DROP TABLE IF EXISTS Teachers;
 CREATE TABLE Teachers (
     id SERIAL PRIMARY KEY,
-    fuul_name VARCHAR(150) NOT NULL
-    
+    full_name VARCHAR(150) NOT NULL
+
 );
 
 -- Створення таблиці предметів
@@ -29,15 +30,17 @@ DROP TABLE IF EXISTS Subjects;
 CREATE TABLE Subjects (
     id SERIAL PRIMARY KEY,
     name VARCHAR(175) NOT NULL,
-    teacher_id INTEGER REFERENCES Teachers(id)  
+    teacher_id INTEGER REFERENCES Teachers(id)
+      on delete cascade
 );
 
 -- Створення таблиці оцінок
 DROP TABLE IF EXISTS Grades;
 CREATE TABLE Grades (
     id SERIAL PRIMARY KEY,
-    student_id INTEGER REFERENCES Students(id),
-    subject_id INTEGER REFERENCES Subjects(id),
+    student_id INTEGER REFERENCES Students(id) ON DELETE CASCADE,
+    subject_id INTEGER REFERENCES Subjects(id) ON DELETE CASCADE,
     grade INTEGER CHECK (grade >= 0 and grade <= 100),
     grade_date DATE NOT NULL
 );
+
